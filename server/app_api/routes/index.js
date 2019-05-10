a4829f4bc5bb5a07cb720b74125ae601ce5a11ab
zzphoghy <doivjpxx@gmail.com>
2019-05-10 23:12:58 +0700

    add some api

--- server/app_api/routes/index.js
+++ server/app_api/routes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const articles = require('./articles');
 const users = require('./users');
+const images = require('./images');
 
 //Users
 router.use('/v1/users', users);
 
 //Articles
 router.use('/v1/articles', articles);
+
+//Images
+router.use('/v1/images', images);
 
 module.exports = router;
